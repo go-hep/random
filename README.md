f02ca77d8b338851a69f3dd91e8d609deb835104
Sebastien Binet <binet@cern.ch>
2013-03-27 10:52:51 +0100

    first import

---
+++ README.md
@@ -0,0 +1,15 @@
+go-hep/rand
+===========
+
+``rand`` is a set of random distribution functions (continous and discrete ones).
+
+## Installation
+
+```sh
+$ go get github.com/go-hep/rand
+```
+
+## Documentation
+
+http://godoc.org/github.com/go-hep/rand
+
---
+++ rand.go
@@ -0,0 +1,113 @@
+package rand
+
+import (
+	"math"
+	rnd "math/rand"
+)
+
+// Dist is a distribution function
+type Dist func() float64
+
+// DiscrDist a discrete distribution function
+type DiscrDist func() int64
+
+// Gauss returns a normally distributed float64 in the range 
+// [-math.MaxFloat64, +math.MaxFloat64] with standard normal distribution of 
+// mean=mean and stddev=stddev.
+func Gauss(mean, stddev float64, src *rnd.Source) Dist {
+	fct := func() float64 {
+		return rnd.NormFloat64()*stddev + mean
+	}
+	if src != nil {
+		r := rnd.New(*src)
+		fct = func() float64 {
+			return r.NormFloat64()*stddev + mean
+		}
+	}
+	return Dist(fct)
+}
+
+// Exp returns an exponentially distributed float64
+func Exp(mean float64, src *rnd.Source) Dist {
+	fct := func() float64 {
+		return rnd.ExpFloat64() / mean
+	}
+	if src != nil {
+		r := rnd.New(*src)
+		fct = func() float64 {
+			return r.ExpFloat64() / mean
+		}
+	}
+	return Dist(fct)
+}
+
+// Chi2 returns a Chi2 distributed random number generation function
+func Chi2(ndf int64, src *rnd.Source) Dist {
+	norm := Gauss(0, 1, src)
+	fct := func() float64 {
+		x := 0.
+		for i := int64(0); i < ndf; i++ {
+			n := norm()
+			x += n * n
+		}
+		return x
+	}
+	return Dist(fct)
+}
+
+// Poisson
+func Poisson(mean float64, src *rnd.Source) DiscrDist {
+	flat := Flat(0, 1, src)
+	fct := func() int64 {
+		i := int64(0)
+		t := math.Exp(-mean)
+		p := 1.0
+		for ; p > t; p *= flat() {
+			i += 1
+		}
+		return i
+	}
+	return DiscrDist(fct)
+}
+
+// Flat
+func Flat(min, max float64, src *rnd.Source) Dist {
+	delta := max - min
+	fct := func() float64 {
+		return rnd.Float64()*delta + min
+	}
+	if src != nil {
+		r := rnd.New(*src)
+		fct = func() float64 {
+			return r.Float64()*delta + min
+		}
+	}
+	return Dist(fct)
+}
+
+// Bernoulli
+func Bernoulli(p float64, src *rnd.Source) DiscrDist {
+	uniform := Flat(0., 1., src)
+	fct := func() int64 {
+		if uniform() < p {
+			return 1
+		}
+		return 0
+	}
+	return DiscrDist(fct)
+}
+
+// Binomial
+func Binomial(n int64, p float64, src *rnd.Source) DiscrDist {
+	b := Bernoulli(p, src)
+	fct := func() int64 {
+		x := int64(0)
+		for i := int64(0); i <= n; i++ {
+			x += b()
+		}
+		return x
+	}
+	return DiscrDist(fct)
+}
+
+// EOF
